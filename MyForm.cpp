7232f4db48bc88b835376afe0113e3607909ea8a
Vanraj123 <140373632+Vanraj123@users.noreply.github.com>
2023-09-09 16:58:53 +0530

    Calculator Source file
    All Calculator file

---
+++ MyForm.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	calculator2::MyForm form;
+	Application::Run(% form);
+}
+
